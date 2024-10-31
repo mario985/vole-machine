de02fdfea455f559aa16eb42690f8a310069ea97
Mohamed Ashraf <157126553+0MohamedAshraf@users.noreply.github.com>
2024-11-01 01:44:01 +0200

    Update Set_Instruction.h

--- Set_Instruction.h
+++ Set_Instruction.h
@@ -11,11 +11,15 @@ class Set_Instruction :ALU{
     private:
     string Input;
     public:
-    void ChooseInstruction(string & input, Registers &Reg , Memory &Mem ,  string*&ptr);
-    void Load(Registers &Regs , Memory &Mem);//1058 == R0 =M58    2058 == R0 = 58
-    void Store(Registers &Regs , Memory &Mem);//3158  == M58 = R1
-    void Move(Registers &Regs);//4056 == R6 = R5
-    bool Jump(Registers &Regs);// B543  If(R5 == R0) jump to M43
-    void twosCompAdd(Registers &R);
+     void ChooseInstruction(string & input, Registers &Reg , Memory &Mem ,  int ptr);
+     void Load(Registers &Regs , Memory &Mem);//1058 == R0 =M58    2058 == R0 = 58
+     void Store(Registers &Regs , Memory &Mem);//3158  == M58 = R1
+     void Move(Registers &Regs);//4056 == R6 = R5
+     bool Jump(Registers &Regs);// B543  If(R5 == R0) jump to M43
+     void twosCompAdd(Registers &R);
+     void BitOR (Registers &Regs );
+     void BitAND (Registers &Regs );
+     void BitXOR (Registers &Regs );
+
 };
 #endif
