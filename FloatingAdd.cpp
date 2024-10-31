c5a34f0982afc4fe4867fa3548950cf0f3026cd6
Youssef Hossam <155332489+YoussefHosssam@users.noreply.github.com>
2024-11-01 00:50:06 +0200

    Update FloatingAdd.cpp V3

--- FloatingAdd.cpp
+++ FloatingAdd.cpp
@@ -5,10 +5,10 @@
 #include <cmath>
 #include <iomanip>
 #include <sstream>
-using namespace std ;
+using namespace std;
 double binaryFractionToDecimal(const string& binaryFraction) {
     double decimalValue = 0.0;
-    int pointPosition = binaryFraction.find('.');
+    size_t pointPosition = binaryFraction.find('.');
     if (pointPosition == string::npos) {
         return 0.0;
     }
@@ -19,9 +19,9 @@ double binaryFractionToDecimal(const string& binaryFraction) {
     }
     return decimalValue;
 }
-string decimalToBinary(int integerPart, bool isNegative,bool ch) {
-    if (!ch){
-        return "0" ;
+string decimalToBinary(int integerPart, bool isNegative, bool ch) {
+    if (!ch) {
+        return "0";
     }
     string binary = "";
     if (isNegative) {
@@ -54,7 +54,8 @@ string fractionalToBinary(double fractionalPart) {
         if (fractionalPart >= 1) {
             binary += "1";
             fractionalPart -= 1;
-        } else {
+        }
+        else {
             binary += "0";
         }
         count++;
@@ -70,16 +71,16 @@ string convertToBinary(double number) {
     if (isNegative) {
         number = -number;
     }
-    
+
 
     int integerPart = static_cast<int>(number);
     double fractionalPart = number - integerPart;
-bool check = 0 ;
-    if (integerPart){
-        check = 1 ;
+    bool check = 0;
+    if (integerPart) {
+        check = 1;
     }
-    binary += decimalToBinary(integerPart, isNegative , check);
-    
+    binary += decimalToBinary(integerPart, isNegative, check);
+
     if (fractionalPart > 0) {
         binary += fractionalToBinary(fractionalPart);
     }
@@ -87,85 +88,87 @@ bool check = 0 ;
     return binary;
 }
 int normalizeBinary(string& binary) {
-    int exponent = 0;
+    size_t exponent = 0;
     string normalized;
     // Find the position of the first '1'
-    unsigned firstOne = binary.find('1');
-    if (binary.length() == 1 && binary[0] == '0'){
+    size_t firstOne = binary.find('1');
+    if (binary.length() == 1 && binary[0] == '0') {
         firstOne = 0;
     }
     if (binary.find('.') != string::npos) {
         // Case when there is a decimal point in the binary string
-        int decimalPos = binary.find('.');
+        size_t decimalPos = binary.find('.');
         if (firstOne < decimalPos) {
             // Example: 0.1101 -> 1.101 * 2^-1
-            exponent = -(decimalPos - firstOne);
-        } else {
+            exponent = (decimalPos - firstOne);
+        }
+        else {
             // Example: 1.101 -> already normalized
             exponent = firstOne - decimalPos;
         }
-    } else {
+    }
+    else {
         // Case when it's a binary integer (e.g., 00111110)
         exponent = binary.length() - firstOne - 1;
     }
     // Move the first `1` to the start, and insert a decimal point
     normalized = binary.substr(firstOne, binary.length() - firstOne);
     normalized.insert(1, ".");
-     if (binary[1] == '.'){
-        exponent*=-1;
+    if (binary[1] == '.') {
+        exponent *= -1;
     }
-    binary = normalized ;
-    return exponent;
+    binary = normalized;
+    return (int)exponent;
 }
-string decimalToHex(int decimal) { 
-    stringstream ss; 
-    ss << hex << uppercase << decimal; 
-    return ss.str(); 
+string decimalToHex(int decimal) {
+    stringstream ss;
+    ss << hex << uppercase << decimal;
+    return ss.str();
 }
-void FloatAdd (string H1 , string H2){
+void FloatAdd(string H1, string H2) {
     string bnr1 = bitset<8>(stoul(H1, nullptr, 16)).to_string();
     string bnr2 = bitset<8>(stoul(H2, nullptr, 16)).to_string();
-    int sgn1 = stoi(bnr1.substr(0,1));
-    int sgn2 = stoi(bnr2.substr(0,1));
-    int exp1 = bitset<8>(bnr1.substr(1,3)).to_ulong();
-    int exp2 = bitset<8>(bnr2.substr(1,3)).to_ulong();
-    string mantesa1 = "0."+bnr1.substr(4,8);
+    int sgn1 = stoi(bnr1.substr(0, 1));
+    int sgn2 = stoi(bnr2.substr(0, 1));
+    int exp1 = bitset<8>(bnr1.substr(1, 3)).to_ulong();
+    int exp2 = bitset<8>(bnr2.substr(1, 3)).to_ulong();
+    string mantesa1 = "0." + bnr1.substr(4, 8);
     double man1 = binaryFractionToDecimal(mantesa1);
-    string mantesa2 = "0."+bnr2.substr(4,8);
+    string mantesa2 = "0." + bnr2.substr(4, 8);
     double man2 = binaryFractionToDecimal(mantesa2);
-    double res1 = pow(-1,sgn1) * pow(2,exp1-4) * double(1+man1);
-    double res2 = pow(-1,sgn2) * pow(2,exp2-4) * double(1+man2);
-    double res3 = res1+res2 ;
-    string sgn3 = "0"; 
-    if ((res3 < 0)){
+    double res1 = pow(-1, sgn1) * pow(2, exp1 - 4) * double(1 + man1);
+    double res2 = pow(-1, sgn2) * pow(2, exp2 - 4) * double(1 + man2);
+    double res3 = res1 + res2;
+    string sgn3 = "0";
+    if ((res3 < 0)) {
         sgn3 = '1';
-        res3 = -1 * res3 ;
+        res3 = -1 * res3;
     }
-    string bnr3 = convertToBinary(res3) ;
-    int exp3 = normalizeBinary(bnr3) ;
-    if (exp3 + 3 > 7){
-        exp3 = 3 ;
+    string bnr3 = convertToBinary(res3);
+    int exp3 = normalizeBinary(bnr3);
+    if (exp3 + 3 > 7) {
+        exp3 = 3;
     }
-    string mantes3 ;
+    string mantes3;
 
-    for (int i = 0 ; i < bnr3.length() ; i++ ){
-        if (bnr3[i] == '.'){
-            mantes3 = bnr3.substr(i+1 ,i+3 );
+    for (int i = 0; i < bnr3.length(); i++) {
+        if (bnr3[i] == '.') {
+            mantes3 = bnr3.substr(i + 1, i + 3);
             break;
         }
     }
-    while (mantes3.length() < 4){
-        mantes3 = mantes3 +='0';
+    while (mantes3.length() < 4) {
+        mantes3 = mantes3 += '0';
     }
-    string finalbnr = sgn3 + bitset<3>(exp3+4).to_string() + mantes3 ;
-    stringstream ss; 
+    string finalbnr = sgn3 + bitset<3>(exp3 + 4).to_string() + mantes3;
+    stringstream ss;
     ss << uppercase << hex << stoi(finalbnr, nullptr, 2);
     string hex = ss.str();
-    cout << hex << " " ;
+    cout << hex << " ";
 
-    
+
 
 }
 int main() {
-FloatAdd("A9", "A1");
+    FloatAdd("A", "A");
 }
