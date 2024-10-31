c27e7d0332488f2bd19d62ce6a3b1aaef30ca625
Mohamed Ashraf <157126553+0MohamedAshraf@users.noreply.github.com>
2024-11-01 01:43:06 +0200

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -11,28 +11,48 @@
 #include <iomanip>
 #include <regex>
 #include <fstream>
+
 using namespace std;
-class CPU:public ALU{
-    private:
+class CPU {
+
+protected:
     Memory Mem;
     Registers Reg;
     vector<string>file_content;
-    string *Program_counter;
-    string Input;
+    int Program_counter;
+    string IR;
     Set_Instruction Inst;
-    public:
-    CPU();
-    bool ChooseMethod(const char & Choice);
-    void Import(string filename);
-    void RunCode();
-    void Step();
-    void ClearMemory();
-    void ClearRegister();
-    void Clear();
-    void print();
-    bool Is_Valid_Input(const string&name);
-    bool File_Check(const string&name);
+    ALU AL;
 
+public:
+    CPU();
+        //bool ChooseMethod(const char & Choice);
+        void RunCode();
+        void Step();
+        void Import(string filename);
+        //void ClearMemory();
+        void ClearRegister();
+        //void Clear();
+        //void print();
+        bool Is_Valid_Input(const string&name);
+        bool File_Check(const string&name);
+        Memory& getMem() {
+            return Mem;
+        }
+        string& getIR() {
+            return IR;
+        }
+        ALU& getALU() {
+
+            return AL;
+        }
+        Registers& getReg() {
+
+            return Reg;
+        }
+        int& getPc() {
+            return Program_counter;
+        }
 
 };
 #endif
